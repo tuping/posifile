d91bd7821583191349eced89836958d0fac49d8a
marcofognog <marcofognog@gmail.com>
2011-03-28 13:14:05 -0300

    cleaning up a bit

--- tests_posifile.rb
+++ tests_posifile.rb
@@ -4,14 +4,16 @@ require 'test_helpers'
 
 class Client < Posifile 
 	set_data_file "sample.txt"
-	set_specification({"name"=>0..10, 
+	set_specification("name"=>0..10, 
 											"city"=>11..31,
 											"country"=>32..42
-										})
+										)
 end
 
 class TestPosifile < Test::Unit::TestCase
+
 	include TestHelpers
+
 	def setup
 		create_sample_file
 	end
@@ -19,14 +21,12 @@ class TestPosifile < Test::Unit::TestCase
 	def test_posifile
 		client = Client.new
 		client.build_attriubutes_from_hash
-#		puts client.name
 		assert_equal "jose", client.name
 	end
 
 	def test_posifile2
 		client = Client.new
 		client.build_attriubutes_from_hash
-#		puts "cidade: "+client.name
 		assert_equal "sorocaba", client.city
 	end
 
