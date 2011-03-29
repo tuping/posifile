c53ade7fcf70105b2bc370ec2156b76226c22609
marcofognog <marcofognog@gmail.com>
2011-03-29 17:12:11 -0300

    forgot this file in the last commit

--- lib/posifile.rb
+++ lib/posifile.rb
@@ -3,9 +3,33 @@ class Posifile
 
 	attr_accessor :data_file
 
+
+#experimental
+	@@var = ''
+
+	def self.var
+		@@var
+	end
+
+#experimental
+
+	def initialize(data_file_name)
+		@data_file = data_file_name
+
+		#faça uma leitura linha a linha  e chame o build_attributes passando qual vai ser o specificatoin
+			build_attriubutes_from_hash(@@specification[self.class])
+	end
+
+
 	def self.set_specification(hash)
 		@@specification[self] ||= {}
 		@@specification[self] = hash
+	end
+
+	def self.lines_where(range,value,&block)
+		#armazene os dois specifications passados
+		@@var = "001color"
+		yield
 	end
 
 	def self.valid?(file_name)
@@ -64,21 +88,17 @@ class Posifile
 		higher_number
 	end
 
-	def initialize(data_file_name)
-		@data_file = data_file_name
-		build_attriubutes_from_hash
-	end
 
 	def file_content
 		file = File.open(@data_file,"r")
-		file.readline
+		file.readlines[0]
 	end
 
-	def field_value(field_name)
+	def field_value(field_name,specification_hash)
 		content = file_content
 		content_ar = content.split('')
 		value_str = ''
-		range = @@specification[self.class][field_name]
+		range = specification_hash[field_name]
 		range.each do |n|
 				value_str.concat content_ar[n]
 		end
@@ -92,11 +112,12 @@ class Posifile
 	end
 
 
-	def build_attriubutes_from_hash
-		@@specification[self.class].each do |key, not_used|
+	def build_attriubutes_from_hash(specification_hash)
+		#receba um specification como argumento
+		specification_hash.each do |key, not_used|
 			self.instance_eval "
 				def #{key}
-					\"#{field_value(key)}\"
+					\"#{field_value(key,specification_hash)}\"
 				end
 			"
 		end
