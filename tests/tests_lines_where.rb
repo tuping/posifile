53c3a4975fd94724a88883a65d8c3a11cf3fff8b
marcofognog <marcofognog@gmail.com>
2011-03-30 15:28:56 -0300

    changing the message, temporarily

--- tests/tests_lines_where.rb
+++ tests/tests_lines_where.rb
@@ -18,9 +18,12 @@ class TestLinesWhere < Test::Unit::TestCase
 
 	def test_lines_where_two_lines_two_specs
 		car = MultiLinesWithTwoSpecs.new("samples/multi_line_sample.txt")
-		assert_equal "fusca", car.carros
-		assert_equal "BEGE", car.color
-		assert_equal "harley", car.motos
+		assert_equal car.fusca.class == Hash
+		assert_equal car.uno.class == Hash
+		assert_equal "fusca", car.fusca["name"]
+		assert_equal "uno", car.uno["name"]
+		assert_equal "yamaha", car.yamaha["name"]
+		assert_equal "blue", car.yamaha["color"]
 	end
 
 	def test_lines_where_two_lines_one_spec_yellow
