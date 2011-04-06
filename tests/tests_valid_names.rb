705eadee8ada35ed9fa2570e42c140771bbed7e0
marcofognog <marcofognog@gmail.com>
2011-04-06 13:50:29 -0300

    added more tests to valid_name?

--- tests/tests_valid_names.rb
+++ tests/tests_valid_names.rb
@@ -9,13 +9,6 @@ class TestsCheckForUppercase < Test::Unit::TestCase
 
 	include TestHelpers
 
-	def setup
-	end
-
-	def tests_valid_names_false
-		assert !UppercasedSpec.valid_names?({"Name"=>1..10, "CITY"=>11..20, "country"=>21..32})
-	end
-
 	def tests_valid_names_true
 		assert UppercasedSpec.valid_names?({"name"=>1..10, "city"=>11..20, "country"=>21..32})
 	end
@@ -24,7 +17,23 @@ class TestsCheckForUppercase < Test::Unit::TestCase
 		assert UppercasedSpec.valid_names?("nome_do_arquivo"=>2..14,"codigo_de_origem"=>15..22,"data_de_geracao_do_arquivo"=>23..30)
 	end
 
+	def tests_valid_names_false
+		assert !UppercasedSpec.valid_names?({"Name"=>1..10, "CITY"=>11..20, "country"=>21..32})
+	end
+
 	def tests_valid_names_false2
 		assert !UppercasedSpec.valid_names?("nome do arquivo"=>2..14,"codigo"=>15..22,"data"=>23..30)
+	end
+
+	def tests_valid_names_false3
+		assert !UppercasedSpec.valid_names?({"Name"=>1..10, "last name"=>11..20, "country"=>21..32})
+	end
+
+	def tests_valid_names_false4
+		assert !UppercasedSpec.valid_names?({"name"=>1..10, "last-name"=>11..20, "country"=>21..32})
+	end
+
+	def tests_valid_names_false5
+		assert !UppercasedSpec.valid_names?({"name*"=>1..10})
 	end
 end
